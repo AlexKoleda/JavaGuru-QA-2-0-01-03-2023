5616097c4feed38746655cf3624c99e8c246bfbf
AlexKoleda <git@github.com:kategulbinova/JavaGuru-QA-2-0-01-03-2023.git>
2023-04-19 16:11:35 +0300

    Homeworks

---
+++ AleksandraK/HelloWorld.java
@@ -0,0 +1,11 @@
+package AleksandraK;
+
+public class HelloWorld {
+    String hello = "Hello World";
+    String txt = "Hello World";
+    int stringLength;
+    stringLength = hello.length();
+    System.out.println(txt.toUpperCase();
+    System.out.println(txt.toLowercase());
+
+}
